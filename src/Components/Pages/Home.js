5fd1e55cf64abbec128b590035d1a4ef14b70eda
“renatowillon” <renatowillon@hotmail.com>
2024-01-22 11:15:59 -0300

    Responsividade home - container

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,14 +2,17 @@ import style from './Home.module.css';
 import savings from '../../img/savings.svg';
 import LinkButton from '../layout/LinkButton';
 
+
 function Home(){
     return(
+     
         <section className={style.home_container}>
             <h1> Bem Vindo ao <span>wDev | Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <LinkButton to="/NewProject" text="Criar Projeto"/>
             <img src={savings} alt="Costs" />
         </section>
+     
 );
 }
 
